da7dc685b8fb3f91d6fc7da4ab9c94df7c354650
manuelAW99 <mvilasvaliente@gmail.com>
2021-03-07 00:08:55 -0600

    capaF

--- cf.py
+++ cf.py
@@ -1,57 +1,60 @@
-class parser():
+def run():
+    file = open("script.txt","r")
+    return parser(file)
 
-    def run(self):
-        file = open("script.txt","r")
-        return parser(file)
+def create_hub(command):
+    print(command[2:])
+    pass
+def create_host(command):
+    print(command[2:])
+    pass  
+def connect(command):
+    print(command[2:])
+    pass  
+def send(command):
+    pass    
+def disconnect(command):
+    pass  
 
-    def parser(self,file):
-        def create_hub(self,command):
-            return(command[2:])
-            
-        def create_host(self,command):
-            print(command[2:])
-            # make_create_host(t, elm1, elm2)
-            pass  
-        def connect(self,command):
-            print(command[2:])
-            pass  
-        def send(self,command):
-            pass    
-        def disconnect(self,command):
-            pass  
-        commands = []
-        current = 0
-        while True:
-            line = file.readline()
-            if not line: break
-            commands.append(line.split())
-            if commands[current][1] == "create":
-                if commands[current][2] == "hub":
-                    create_hub(commands[current])
-                else: 
-                    create_host(commands[current])
-            elif commands[current][1] == "connect":
-                connect(commands[current])
-            elif commands[current][1] == "send":
-                send(commands[current])
-            else:
-                disconnect(commands[current])
-            current += 1
-        return commands
-
+def parser(file):
+    commands = []
+    current = 0
+    while True:
+        line = file.readline()
+        if not line: break
+        commands.append(line.split())
+        if commands[current][1] == "create":
+            if commands[current][2] == "hub":
+                create_hub(commands[current])
+            else: 
+                create_host(commands[current])
+        elif commands[current][1] == "connect":
+            connect(commands[current])
+        elif commands[current][1] == "send":
+            send(commands[current])
+        else:
+            disconnect(commands[current])
+        current += 1
+    return commands
 
 class cable():
     def __init__(self,port1,port2):
-        self.port1 = port1
-        self.port2 = port2
-        self.status = "null"
+        port1 = port1
+        port2 = port2
+        status = "null"
         
 class cumputer():
     def __init__(self,name):
-        self.name = name
+        name = name
         
         
 class hub():
     def __init__(self,name,c_ports):
-        self.name = name 
-        self.c_ports = c_ports
+        name = name 
+        c_ports = c_ports
+
+a = hub("h",2)
+if type(a) is hub:
+    print(1)
+else: print(0)
+run()
